c832f448d96836359e672fabdaa2f8ac0bf15855
Edgaras Avagyan <35609247+Crownley@users.noreply.github.com>
2020-07-13 00:33:36 +0300

    Adding UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getUserData() as [$email, $password])
+        {
+            $user = new User();
+            $user->setEmail($email);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+
+    private function getUserData(): array
+    {
+        return [
+
+            ['have@great.day', '123456'],
+            ['email@fortesting.it', '123456'],
+            ['test@test.com', '123456']
+
+        ];
+    }
+}
