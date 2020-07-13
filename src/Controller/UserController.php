1260516dc8625f482dbb68ace1a2cd541cf06cc8
Edgaras Avagyan <35609247+Crownley@users.noreply.github.com>
2020-07-13 07:15:46 +0300

    Adding few phpUnit tests

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,63 +2,30 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 use App\Service\Entity\UserService;
-use App\Validator\EntityValidator;
 
 class UserController extends AbstractController
 {
-
-    
     /**
     * @Route("/register", name="api_register", methods={"POST"})
     */
-    public function register(EntityManagerInterface $om, UserPasswordEncoderInterface $passwordEncoder, Request $request, EntityValidator $validator)
-    {
-        $user = new User();
-        $email                  = $request->request->get("email");
-        $password               = $request->request->get("password");
-        $passwordConfirmation   = $request->request->get("password_confirmation");
-        $errors = [];
-        if($password != $passwordConfirmation)
-        {
-            $errors[] = "Password does not match the password confirmation.";
-        }
-        if(strlen($password) < 6)
-        {
-            $errors[] = "Password should be at least 6 characters.";
-        }
-        if(!$errors)
-        {
-            $encodedPassword = $passwordEncoder->encodePassword($user, $password);
-            $user->setEmail($email);
-            $user->setPassword($encodedPassword);
-            $isValid = $validator->validate($user);
-            if($isValid)
-            {
-                // Save entity
-                $om->persist($user);
-                $om->flush();
-                return $this->json([
-                    'user' => $user
-                ]);
-            }
-            else
-            {
-                $errors = $validator->getErrors();
-            }
-        }
-                
-        return $this->json([
-            'errors' => $errors
-        ], 400);
-    }
+    public function register(Request $request, UserService $userService)
+{
+   if($userService->create($request->request->all()))
+   {
+       return $this->json([
+           'user' => $userService->getUser()
+       ]);
+   }
+   return $this->json([
+       'errors' => $userService->getErrors()
+   ], 400);
+}
 
     /**
     * @Route("/login", name="api_login", methods={"POST"})
---
+++ tests/ApiTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\ClientException;
+
+class ApiTest extends TestCase
+{
+    
+    public function testMainRoute()
+    {
+        $client = new Client();
+        $response = $client->request('GET', 'localhost:8000/adverts');
+
+        $this->assertSame($response->getStatusCode(), 200);
+    }
+
+
+    public function testLogin()
+    {
+        $client = new Client();
+
+        $response = $client->request('POST', 'localhost:8000/login', [
+            'form_params' => [
+                'email' => 'test@test.com',
+                'password' => '123456'
+            ]
+        ]);
+        $this->assertSame($response->getStatusCode(), 200);
+    }
+
+
+    public function testDeleteAdvert()
+    {   
+        $client = new Client();
+        try {
+            $client->request('DELETE', 'localhost:8000/adverts/177');
+        } catch (ClientException $e) {
+            $response = Psr7\str($e->getResponse());
+        }
+        if( strpos($response, 'Access Denied') !== false) {
+            $value = true;
+        } else {
+            $value = false;
+        }
+        $this->assertTrue($value);
+    }
+}
